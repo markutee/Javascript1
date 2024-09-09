19e0128881d7b4569b08447b5ec1e3593ecc97d2
markutee <146543647+markutee@users.noreply.github.com>
2024-09-09 10:12:51 +0300

    Add files via upload

---
+++ workshop 2/koodi.js
@@ -0,0 +1,13 @@
+// tehtävä 1
+document.write("<p><cite><strong>If I had nine hours to chop down a tree, I'd spend the first six sharpening my ax.</strong> -- Abraham Lincoln</cite></p>");
+
+// tehtävä 2
+function repeatingText(text) {
+    for (let i = 0; i < 50; i++) {
+        document.write(text + "<br>");
+    }
+}
+
+repeatingText("Tämä on toistuva lause.");
+
+//tehtävä 3
